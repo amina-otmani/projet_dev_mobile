338da21d99ff884391d0cafd422698f858b35513
theo <theo.constantin01@etu.umontpellier.fr>
2026-03-24 11:28:49 +0100

    add editeur and jeu dto

---
+++ frontend/app/src/main/java/com/example/projet_dev_mobile/data/network/dto/EditeurDTO.kt
@@ -0,0 +1,20 @@
+package com.example.projet_dev_mobile.data.network.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class EditeurDto(
+    val id: Int,
+    val nom: String,
+    val created_at: String? = null,
+    val contacts: List<EditeurContactDto> = emptyList()
+)
+
+@Serializable
+data class EditeurContactDto(
+    val id: Int,
+    val nom: String,
+    val prenom: String,
+    val email: String,
+    val poste: String? = null
+)
